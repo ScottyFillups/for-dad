bfa4b7c239caaf61484dbdc546af57cb3701f98f
Philip Scott <pscott@zeptohost.com>
2020-07-15 16:53:11 -0400

    forgot url, woops

--- api/peterborough.js
+++ api/peterborough.js
@@ -4,6 +4,7 @@ const url = 'http://wxmaps.org/cgi-bin/draw_meteogram'
 
 module.exports = (req, res) => {
   axios({
+    url,
     method: 'POST',
     headers: {'content-type': 'text/plain;charset=UTF-8' },
     data: "<gaplot><field><lon>-78.3</lon><lat>44.3</lat><model>0</model><unit>1</unit></field></gaplot>",
