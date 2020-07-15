4e4879b3205bdb37ba9455a5dade14d82eb72d07
Philip Scott <pscott@zeptohost.com>
2020-07-15 17:00:25 -0400

    yolo pipe to response

--- api/peterborough.js
+++ api/peterborough.js
@@ -9,5 +9,15 @@ module.exports = (req, res) => {
     headers: {'content-type': 'text/plain;charset=UTF-8' },
     data: "<gaplot><field><lon>-78.3</lon><lat>44.3</lat><model>0</model><unit>1</unit></field></gaplot>",
     responseType: 'text'
-  }).then(r => res.send(r.data))
+  }).then(r => {
+    const img = r.data
+    axios({
+      url: img,
+      method: 'GET',
+      responseType: 'stream'
+    }).then(r => {
+      res.header('content-type', 'image/png')
+      r.data.pipe(res)
+    })
+  })
 }
